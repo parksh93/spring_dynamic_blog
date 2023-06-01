5cc867c2a7d6daa2a0146b71c5b3be4f84a3d322
parksh93 <sanghyun8527@gmail.com>
2023-06-01 17:26:49 +0900

    spring blog CRUD완성

--- src/main/java/com/spring/blog/repository/BlogRepository.java
+++ src/main/java/com/spring/blog/repository/BlogRepository.java
@@ -19,4 +19,14 @@ public interface BlogRepository {
 
     // 전체 데이터 조회
     List<Blog> findAll();
+
+    Blog findById(long blogId);
+
+    void save(Blog blog);
+
+    void deleteById(long blogId);
+
+    //JPA에서는 .save()를 동일하게 쓰지만, 현재 코드에서 메서드 오버로딩도 불가능하고
+    //분리할 방법이 없으므로 메서드명을 다르게 사용한다
+    void update(Blog blog);
 }
--- src/test/java/com/spring/blog/repository/BlogRepositoryTest.java
+++ src/test/java/com/spring/blog/repository/BlogRepositoryTest.java
@@ -1,16 +1,13 @@
 package com.spring.blog.repository;
 
 import com.spring.blog.entity.Blog;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) //DROP TABLE시 필요한 어노테이션
@@ -26,10 +23,81 @@ public class BlogRepositoryTest {
 
     @Test
     public void findAllTest(){
+        int blogRow = 1;
+
         List<Blog> blogList = blogRepository.findAll();
 
-        System.out.println(blogList);
         assertEquals(3, blogList.size());
+        assertEquals(2,blogList.get(blogRow).getBlogId());
+    }
+
+    @Test
+    @DisplayName("2번 글의 작성자, 제목, id 검사")
+    public void findByIdTest(){
+        int blogId = 2;
+
+        Blog blog = blogRepository.findById(blogId);
+
+        assertEquals("2번유저",blog.getWriter());
+        assertEquals("2번제목",blog.getBlogTitle());
+        assertEquals(2,blog.getBlogId());
+    }
+
+    @Test
+    public void saveTest(){
+        String writer = "추가유저";
+        String blogTitle = "추가글제목";
+        String blogContent = "추가글내용";
+
+//        Blog blog = new Blog();
+//        blog.setWriter(writer);
+//        blog.setBlogTitle(blogTitle);
+//        blog.setBlogContent(blogContent);
+        //빌더패턴
+        Blog blog = Blog.builder()
+                .writer(writer)
+                .blogTitle(blogTitle)
+                .blogContent(blogContent)
+                .build();
+
+        blogRepository.save(blog);
+        List<Blog> blogList = blogRepository.findAll();
+
+        int blogRow = 3;
+
+        assertEquals(4, blogList.size());
+        assertEquals(writer,blogList.get(blogRow).getWriter());
+    }
+
+    @Test
+    @DisplayName("2번 글을 삭제 후 남은 데이터가 2개인지 검사")
+    public void deleteByIdTest(){
+        long blogId = 2;
+
+        blogRepository.deleteById(blogId);
+//        List<Blog> blogList = blogRepository.findAll();
+//        Blog blog = blogRepository.findById(blogId);
+
+        assertEquals(2, blogRepository.findAll().size());
+        assertNull(blogRepository.findById(blogId));
+    }
+
+    @Test
+    public  void updateTest(){
+        int blogId = 3;
+        String blogTitle = "바꾼제목";
+        String blogContent = "바꾼내용";
+
+        Blog blog = Blog.builder()
+                .blogId(blogId)
+                .blogTitle(blogTitle)
+                .blogContent(blogContent)
+                .build();
+
+        blogRepository.update(blog);
+
+        assertEquals(blogTitle, blogRepository.findById(blog.getBlogId()).getBlogTitle());
+        assertEquals(blogContent, blogRepository.findById(blog.getBlogId()).getBlogContent());
     }
 
     @AfterEach  //각 단위 테스트 끝난 후에 실행할 구문 작성
